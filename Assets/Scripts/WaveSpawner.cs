bac7de2cbad12f58500698058fe93bde80ef18c3
rumkkee <accesareo@gmail.com>
2024-03-18 23:57:43 -0700

    Set a timer for the next spawned wave

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,7 +1,6 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WaveSpawner : MonoBehaviour
 {
@@ -10,6 +9,8 @@ public class WaveSpawner : MonoBehaviour
 
     public float timeBetweenWaves = 5f;
     private float countdown = 2f;
+
+    public TextMeshProUGUI waveCountdownText;
 
     private int waveIndex = 0;
 
@@ -21,6 +22,8 @@ public class WaveSpawner : MonoBehaviour
             countdown = timeBetweenWaves;
         }
         countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Floor(countdown).ToString();
     }
 
     private IEnumerator SpawnWave()
