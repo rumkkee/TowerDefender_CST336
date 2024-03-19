44b092aff3fd692d37f4f85442f16360af11d5c6
rumkkee <accesareo@gmail.com>
2024-03-19 11:22:00 -0700

    Set up Turret and its radius gizmo

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public Transform target;
+    public float range = 15f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
