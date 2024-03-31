86e37910896fea29c9261bd3bd286edb8ef3e25a
rumkkee <accesareo@gmail.com>
2024-03-30 18:37:06 -0700

    Implemented missile functionality
    If the missile collides with its target, it damages any enemy in its range.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,6 +9,7 @@ public class Bullet : MonoBehaviour
 
     public float speed = 70f;
 
+    public float explosionRadius = 0;
     public GameObject impactEffect;
 
     public void Seek(Transform target)
@@ -34,13 +35,47 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(_target);
     }
 
     private void HitTarget()
     {
         Debug.Log("Hit something");
+
+        if(explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(_target);
+        }
+
         Destroy(_target.gameObject);
         Instantiate(impactEffect, transform.position, transform.rotation);
         Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if(collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
