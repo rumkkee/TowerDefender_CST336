7c0ccc2c6a063f85a49ca4715ad663cc8e1d1b22
rumkkee <accesareo@gmail.com>
2024-03-31 20:49:27 -0700

    Player can purchase a Laser Beamer turret

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -6,6 +6,7 @@ public class Shop : MonoBehaviour
 {
     public TurretBlueprint standardTurret;
     public TurretBlueprint missileLauncher;
+    public TurretBlueprint laserBeamer;
 
     BuildManager buildManager;
 
@@ -24,5 +25,11 @@ public class Shop : MonoBehaviour
     {
         Debug.Log("Missile Launcher Purchased");
         buildManager.SelectTurretToBuild(missileLauncher);
+    }
+
+    public void SelectLaserBeamer()
+    {
+        Debug.Log("Laser Beamer Selected");
+        buildManager.SelectTurretToBuild(laserBeamer);
     }
 }
